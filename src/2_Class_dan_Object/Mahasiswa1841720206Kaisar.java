59c33a68dc026dfb7be0260584f3640732eb15be
kaisarwahyuarya <54808586+kaisarwahyuarya@users.noreply.github.com>
2019-09-08 10:52:51 +0700

    Add files via upload

---
+++ src/2_Class_dan_Object/Mahasiswa1841720206Kaisar.java
@@ -0,0 +1,15 @@
+package Jobsheet_2;
+
+public class Mahasiswa1841720206Kaisar {
+    public int nim;
+    public String nama;
+    public String alamat;
+    public String kelas;
+    
+    public void tampilBiodataKaisar(){
+        System.out.println("NIM     : " +nim);
+        System.out.println("Nama    : " +nama);
+        System.out.println("Alamat  : " +alamat);
+        System.out.println("Kelas   : " +kelas);
+    }
+}
---
+++ src/2_Class_dan_Object/TestMahasiswa1841720206Kaisar.java
@@ -0,0 +1,14 @@
+package Jobsheet_2;
+
+public class TestMahasiswa1841720206Kaisar {
+    
+    public static void main(String[] args) {
+        
+        Mahasiswa1841720206Kaisar mhs1 = new Mahasiswa1841720206Kaisar();
+        mhs1.nim = 101;
+        mhs1.nama = "Lestari";
+        mhs1.alamat = "Jl. Vinolia No 1A";
+        mhs1.kelas = "1A";
+        mhs1.tampilBiodataKaisar();
+    }  
+}
